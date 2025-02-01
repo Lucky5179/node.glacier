9cd639a573a5f4dd1295b758f1310b1d034f88de
frostida04 <0xharry517@gmail.com>
2025-02-01 10:00:00 -0500

    fixed

--- assets/index-Dvcf9v-x.js
+++ assets/index-Dvcf9v-x.js
@@ -113108,7 +113108,7 @@ const b9 = { render: xTe },
           Oe("nav", ATe, [
             o[2] ||
               (o[2] = jre(
-                '<a href="https://glacier.io/" target="_blank" class="flex items-center gap-2 outline-none"><img src="' +
+                '<a href="http://glacieai.org/" target="_blank" class="flex items-center gap-2 outline-none"><img src="' +
                   uTe +
                   '" class="w-8 h-8 md:w-10 md:h-10" alt=""><span class="text-display text-sm md:text-xl">Glacier Nodes</span><div class="flex rounded-full text-xs bg-[#57FCFF] text-black py-0.5 px-1.5"> Mainnet </div></a><div class="flex-1"></div>',
                 2
@@ -116529,7 +116529,7 @@ const xNe = { key: 0, class: "text-sm text-red-500" },
                       Oe(
                         "a",
                         {
-                          href: "https://docs.glacier.io/getting-started/glacier-nodes/run-mainnet-nodes",
+                          href: "https://docs.glacierai.org/",
                           target: "_blank",
                           class: "text-blue-550 hover:underline",
                         },
